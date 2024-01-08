f693cbafa3751219e37a6bc53d06bf1285af78d1
sheelendar <sheelendar@Sheelendars-MacBook-Pro.local>
2024-01-08 14:55:38 +0530

    added lld of apply coupon

---
+++ lld_system_design/lld_of_apply_coupon/apply_coupon_app.go
@@ -0,0 +1,11 @@
+package main
+
+/*
+"Given Shopping cart with products and coupons and calculate the net price after applying coupons on products.
+Coupons can be of different types with certain conditions.
+1. N% off that is 10% off for all the individual
+2.0% off on next item
+3.D% off on Nth item of Type T.
+Sequentially wants to apply all the coupons on the cart and get the Total amount."
+
+*/
